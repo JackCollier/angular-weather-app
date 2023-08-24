542db8432e22978d21bc789e62c8b7d23e54f12e
JacvaSipt <steamcow12@hotmail.com>
2023-08-24 13:58:33 +0100

    add form control and call to api

--- src/app/weather-search/weather-search.component.ts
+++ src/app/weather-search/weather-search.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { WeatherApiService } from '../weather-api.service';
 
 @Component({
   selector: 'app-weather-search',
   templateUrl: './weather-search.component.html',
-  styleUrls: ['./weather-search.component.css']
+  styleUrls: ['./weather-search.component.css'],
 })
 export class WeatherSearchComponent {
+  constructor(private weatherApiService: WeatherApiService) {}
 
+  userInputLocationForm = new FormGroup({
+    location: new FormControl(''),
+  });
+
+  submitLocationSearch() {
+    const newLocation = {
+      location: this.userInputLocationForm.value.location ?? '',
+    };
+
+    this.weatherApiService.getWeather(newLocation.location);
+  }
 }
