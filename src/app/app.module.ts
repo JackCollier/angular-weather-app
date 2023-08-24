e2b23d13924554b77d0b713c8d9c2b736f4679c0
JacvaSipt <steamcow12@hotmail.com>
2023-08-24 13:51:02 +0100

    generate component to hold search functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { WeatherSearchComponent } from './weather-search/weather-search.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WeatherSearchComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
