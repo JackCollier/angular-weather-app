add7c217b7e148c2fcda4f13eced15412720725d
JacvaSipt <steamcow12@hotmail.com>
2023-08-24 13:49:46 +0100

    create logic to retrieve weather on location

--- src/app/weather-api.service.ts
+++ src/app/weather-api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherApiService {
+  apiKey: string = 'fc622560fb795dfd8f863f3fab720e48';
+  apiPath: string = `http://api.weatherstack.com/current?access_key=${this.apiKey}&query=`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getWeather(location: string) {
+    return this.http.get(this.apiPath + location);
+  }
 }
