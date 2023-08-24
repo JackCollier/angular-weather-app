3e2deb8849e32750f6188308829862195da9484d
JacvaSipt <steamcow12@hotmail.com>
2023-08-24 14:13:08 +0100

    retrieve input data

--- src/app/weather-data-display/weather-data-display.component.ts
+++ src/app/weather-data-display/weather-data-display.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-weather-data-display',
   templateUrl: './weather-data-display.component.html',
-  styleUrls: ['./weather-data-display.component.css']
+  styleUrls: ['./weather-data-display.component.css'],
 })
 export class WeatherDataDisplayComponent {
-
+  @Input() inputData: any;
 }
